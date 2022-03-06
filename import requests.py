9aad865919ce44aa77850844145a1d8d92356164
DME-WebDev <97695960+DME-WebDev@users.noreply.github.com>
2022-03-06 15:04:26 -0500

    Updated for date/time params

--- import requests.py
+++ import requests.py
@@ -7,6 +7,6 @@ def json_print(obj):
 
 # Pub start/end dates need to be added to URL. Refer to API documentation if the video is unclear. 
 # https://nvd.nist.gov/developers/vulnerabilities
-response = requests.get('https://services.nvd.nist.gov/rest/json/cves/1.0/?keyword=tutor+lms')
+response = requests.get('https://services.nvd.nist.gov/rest/json/cves/1.0/?pubStartDate=2021-08-04T13:00:00:000 UTC-05:00&pubEndDate=2021-10-22T13:36:00:000 UTC-05:00&keyword=tutor+lms')
 
 json_print(response.json())
